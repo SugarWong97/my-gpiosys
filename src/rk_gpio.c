e9dd41fd0b29e95e4097a839555b8c28ebcaebeb
schips <schips@dingtalk.com>
2023-06-26 15:22:28 +0800

    core : 支持多平台（目前只支持RK3588）；提供头文件

--- src/rk_gpio.c
+++ src/rk_gpio.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include "rk_gpio.h"
 
-static int str_to_lower(char * str, int len)
+static int platform = RK3588_GPIO_PLATFORM;
+
+#define DEBUG 1
+#define DEBUG_ERROR 1
+#if DEBUG == 0
+#define rkgpio_debug(format,...)
+#else
+#define rkgpio_debug printf
+#endif
+
+#if DEBUG_ERROR == 0
+#define rkgpio_error(format,...)
+#else
+#define rkgpio_error(format,...) printf
+#endif
+
+static int for_rk_str_to_lower(char * str, int len)
 {
     int i;
     for(i=0;i<len;i++)//遍历
@@ -21,7 +38,7 @@ static int str_to_lower(char * str, int len)
 
 
 /***********************************************
- *  * str_replace
+ *  * _for_rk_str_replace
  *   * 字符串替换
  *    *
  *     * pszInput       要转变的字符串
@@ -31,7 +48,7 @@ static int str_to_lower(char * str, int len)
  *         * nOutputlen 输出缓冲区的长度
  *          *
  *           **********************************************/
-static void str_replace(char * pszInput, char * pszOld, char * pszNew,char * pszOutput,int nOutputlen)
+static void _for_rk_str_replace(char * pszInput, char * pszOld, char * pszNew,char * pszOutput,int nOutputlen)
 {
     char temp[128];
     int nLen=0;
@@ -63,6 +80,17 @@ static void str_replace(char * pszInput, char * pszOld, char * pszNew,char * psz
     return ;
 }
 
+int set_rk_gpio_platform(enum RK_GPIO_PLATFORM pl)
+{
+    if(pl >= RKUNKNOW_GPIO_PLATFORM)
+    {
+        rkgpio_error("[%s] RKUNKNOW_GPIO_PLATFORM %d/%d\n", __func__, pl, RKUNKNOW_GPIO_PLATFORM);
+        return -1;
+    }
+    platform = pl;
+    return 0;
+}
+
 int str_to_rk_gpio(const char * str)
 {
     char str_buff[64]= {0};
@@ -71,23 +99,37 @@ int str_to_rk_gpio(const char * str)
     int bank = 0, pos = 0;
     char group = 0;
     int pin;
+    if(!str)
+    {
+        rkgpio_error("[%s] NULL\n", __func__);
+        return -1;
+    }
+    if(strlen(str) < 3)
+    {
+        rkgpio_error("[%s] string [%s] to short.\n", __func__, str);
+        return -1;
+    }
 
     memset(tmp_str, 0, sizeof(tmp_str));
     strncpy(str_buff, str, strlen(str));
-    printf("Input : %s\n", str_buff);
+    rkgpio_debug("Input : %s\n", str_buff);
 
-    str_to_lower(str_buff, strlen(str_buff));
+    for_rk_str_to_lower(str_buff, strlen(str_buff));
 
-    str_replace(str_buff, "gpio", "", tmp_str, sizeof(tmp_str));
+    memset(tmp_str, 0, sizeof(tmp_str));
+    _for_rk_str_replace(str_buff, "gpio", "", tmp_str, sizeof(tmp_str));
     sprintf(str_buff, "%s", tmp_str);
 
-    str_replace(str_buff, "_", "", tmp_str, sizeof(tmp_str));
+    memset(tmp_str, 0, sizeof(tmp_str));
+    _for_rk_str_replace(str_buff, "_", "", tmp_str, sizeof(tmp_str));
     sprintf(str_buff, "%s", tmp_str);
 
-    str_replace(str_buff, "rk", "", tmp_str, sizeof(tmp_str));
+    memset(tmp_str, 0, sizeof(tmp_str));
+    _for_rk_str_replace(str_buff, "rk", "", tmp_str, sizeof(tmp_str));
     sprintf(str_buff, "%s", tmp_str);
 
-    str_replace(str_buff, "rockchip", "", tmp_str, sizeof(tmp_str));
+    memset(tmp_str, 0, sizeof(tmp_str));
+    _for_rk_str_replace(str_buff, "rockchip", "", tmp_str, sizeof(tmp_str));
     sprintf(str_buff, "%s", tmp_str);
 
 #if 0
@@ -100,17 +142,28 @@ int str_to_rk_gpio(const char * str)
     //ret = sscanf(str_buff, "gpio%d_%c%d", &bank, &group, &pos);
     ret = sscanf(str_buff, "%d%c%d", &bank, &group, &pos);
 
-    pin = bank * 32 + (((group - 'a') * 8) + pos);
-    printf("pin = %d \t= [ bank(%d) * 32 ] + [ group(%c->%d) * 8 ] + pos(%d)\n", pin, bank, group, group - 'a', pos);
-    printf("\t\t= %d + %d + %d\n", bank * 32, (group - 'a') * 8, pos);
+    if(platform == RK3588_GPIO_PLATFORM)
+    {
+        pin = bank * 32 + (((group - 'a') * 8) + pos);
+        rkgpio_debug("pin = %d \t= [ bank(%d) * 32 ] + [ group(%c->%d) * 8 ] + pos(%d)\n", pin, bank, group, group - 'a', pos);
+        rkgpio_debug("\t\t= %d + %d + %d\n", bank * 32, (group - 'a') * 8, pos);
+    }
     return pin;
 }
 
 int test(void)
 {
+
+    set_rk_gpio_platform(RK3588_GPIO_PLATFORM);
+
     str_to_rk_gpio("GPIO1_A0");
-    str_to_rk_gpio("gpio1_a0");
     str_to_rk_gpio("1a0");
+    str_to_rk_gpio("gpio1_a6");
+
+    str_to_rk_gpio("GPIO4_C6");
+    str_to_rk_gpio("gpio4_c6");
+    str_to_rk_gpio("4_c6");
+    str_to_rk_gpio("4c6");
 
     return 0;
 }
