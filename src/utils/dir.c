7cacb507295d88fa6bdd39a6cb4f340777883bd0
schips <schips@dingtalk.com>
2024-08-08 14:08:00 +0800

    utils : dir : 查找目录下含有某个关键字的文件

---
+++ src/utils/dir.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+
+int find_in_dir(char *dirpath, const char *keyword, char* got_filename)
+{
+#if 1
+    int ret = 0;
+    DIR *dir = opendir(dirpath);
+    if(dir == NULL) {
+        printf("open %s failed ",dirpath);
+        perror(":");
+        return;
+    }
+
+    struct dirent *ent;
+    while((ent = readdir(dir)) != NULL)
+    {
+        if(strstr(ent->d_name, keyword) != 0)
+        {
+            //printf("file name: %s \n",ent->d_name);
+            sprintf(got_filename,"%s", ent->d_name);
+            ret = 1;
+            break;
+        }
+    }
+    closedir(dir);
+    return ret;
+#endif
+    //sprintf(got_filename,"gpiochip906");
+    return 1;
+}
---
+++ src/utils/dir.h
@@ -0,0 +1,7 @@
+#ifndef __DIR_H__
+#define __DIR_H__
+
+// 查找目录下含有某个关键字的文件
+int find_in_dir(char *dirpath, const char *keyword, char* got_filename);
+
+#endif/*__DIR_H__*/
