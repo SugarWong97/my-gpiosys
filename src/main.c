259cd7c94703ae1fcefb9e4a816f71ae84317b11
schips <schips@dingtalk.com>
2024-08-08 14:09:35 +0800

    main : 兼容现有平台，能够编译输出

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 #include "gpio.h"
 #include "rk_gpio.h"
 
+//#define io_servalue rk_io_setvalue
+#define io_servalue zynq_7000_io_setvalue
 
 int main(int argc, char * argv[])
 {
@@ -14,10 +16,10 @@ int main(int argc, char * argv[])
     {
         if(argv[2][0] == '1')
         {
-            rk_io_setvalue(argv[1], 1);
+            io_servalue(argv[1], 1);
         }else
         {
-            rk_io_setvalue(argv[1], 0);
+            io_servalue(argv[1], 0);
         }
     }
     return 0;
